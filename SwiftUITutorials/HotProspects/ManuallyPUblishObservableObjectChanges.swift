21ccc3a92801cdc26e3058f16984cbef9dfcedaa
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-19 23:15:44 +0300

    Manually publishing ObservableObject changes

---
+++ SwiftUITutorials/HotProspects/ManuallyPUblishObservableObjectChanges.swift
@@ -0,0 +1,39 @@
+//
+//  ManuallyPUblishObservableObjectChanges.swift
+//  SwiftUITutorials
+//
+//  Created by Ramill Ibragimov on 19.04.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
+struct ManuallyPUblishObservableObjectChanges: View {
+    @ObservedObject var updater = DelayedUpdater()
+    
+    var body: some View {
+        Text("Value is: \(updater.value)")
+    }
+}
+
+struct ManuallyPUblishObservableObjectChanges_Previews: PreviewProvider {
+    static var previews: some View {
+        ManuallyPUblishObservableObjectChanges()
+    }
+}
