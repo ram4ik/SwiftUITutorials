9da22a2c78595967fc114f770da7bd0a574dcde1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-19 16:13:00 +0300

    coreHaptics

---
+++ SwiftUITutorials/Flashzilla/MakingVibrationsWithCoreHaptics.swift
@@ -0,0 +1,64 @@
+//
+//  MakingVibrationsWithCoreHaptics.swift
+//  SwiftUITutorials
+//
+//  Created by Ramill Ibragimov on 19.04.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import CoreHaptics
+import SwiftUI
+
+struct MakingVibrationsWithCoreHaptics: View {
+    @State private var engine: CHHapticEngine?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            //.onTapGesture(perform: simpleSuccess)
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware()
+            .supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware()
+            .supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+}
+
+struct MakingVibrationsWithCoreHaptics_Previews: PreviewProvider {
+    static var previews: some View {
+        MakingVibrationsWithCoreHaptics()
+    }
+}
